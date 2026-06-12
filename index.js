9cc284a60be826da775a82c7b2f21452ed9dda81
Yosimar <edgemovmix@gmailcom>
2026-06-11 21:17:43 -0600

    version de clase

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static(path.join(__dirname, 'views/planetas')));
+app.use(express.static(path.join(__dirname, 'views/img')));
+app.use(express.static(path.join(__dirname, 'views/css')));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.redirect('/menu');
+});
+
+app.get('/menu', (req, res) => {
+    res.render('planetas/menu');
+});
+
+app.get('/mercurio', (req, res) => {
+    res.render('planetas/mercurio');
+});
+
+app.get('/venus', (req, res) => {
+    res.render('planetas/venus');
+});
+
+app.get('/tierra', (req, res) => {
+    res.render('planetas/tierra');
+});
+
+app.get('/marte', (req, res) => {
+    res.render('planetas/marte');
+});
+
+app.get('/jupiter', (req, res) => {
+    res.render('planetas/jupiter');
+});
+
+app.get('/saturno', (req, res) => {
+    res.render('planetas/saturno');
+});
+
+app.get('/urano', (req, res) => {
+    res.render('planetas/urano');
+});
+
+app.get('/neptuno', (req, res) => {
+    res.render('planetas/neptuno');
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
